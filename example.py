234ef99f4d1fbb21c916e715e8bd65af9698bd66
Mihai Damian <damianmihairadu@gmail.com>
2011-07-31 16:55:46 +0300

    Simplified example to mention only mobile authentication

--- example.py
+++ example.py
@@ -5,17 +5,13 @@ from libfm import LibFMError
 lib_fm = LibFM('api key...', 'application secret...')
 
 try:
-    token = lib_fm.get_token()
-    # ... get the user to authorize the token ...
-    # then:
-    lib_fm.create_session(token)
-    # or lib_fm.create_mobile_session('username...', 'password...')
+    lib_fm.create_mobile_session('username...', 'password...')
 
     # simple read
     info = lib_fm.read('artist.getInfo', artist='Pink Floyd')
     print info['artist']['url']
-    for tag in info['artists']['tags']:
-        print tag['tag']['name']
+    for tag in info['artist']['tags']['tag']:
+        print tag['name']
 
     # simple write
     lib_fm.write('artist.addTags', artist='Black Sabbath',
